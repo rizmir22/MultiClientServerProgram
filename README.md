fc524e4b8a642788dccc61a54c04934da2d3504d
rizmir22 <rizmir22@gmail.com>
2017-08-08 07:43:39 +0530

    Innitial commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # MultiClientServerProgram
+
+This is a Multi Client Server Program.
+
+* Multi Client Can be connect to the server using this program
+* In this Server program has a thread pool. maximum number of client to connect to the server is defined by this thread pool Size.
+  in this code thread pool Size is 10.
+* Exceeding client connections are added to the Queue and it will be retrieved when one connected client is diconnected
+
+
+
+* To Run the program you should run the Server first.
+* Then You need to Run the Client
+* make sure the Sevrver host is correct and Port which is going to connect is same as Server listening Port.
+* you will receive a massage that which port you are connected and welcome message.
+* if you want to disconnect then type as quit client sever connection will be disconnected
---
+++ src/main/java/com/wso2/examples/server/Server.java
@@ -0,0 +1,87 @@
+package com.wso2.examples.server;
+
+
+import java.util.*;
+import java.net.*;
+import java.util.concurrent.*;
+import java.io.*;
+
+public class Server implements Runnable {
+    private static final int port = 1978;
+    private final ServerSocket serverSocket;
+    private final ExecutorService pool;
+
+    public Server(int port, int poolSize) throws IOException {
+        serverSocket = new ServerSocket(port);
+        pool = Executors.newFixedThreadPool(poolSize);
+    }
+
+    /**
+     * The main server entry point..
+     */
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(Server.port, 10);
+
+        server.run();
+    }
+
+    public void run() {
+        try {
+            for (; ; ) {
+                pool.execute(new Handler(serverSocket.accept()));
+            }
+        } catch (IOException ex) {
+            pool.shutdown();
+        }
+    }
+
+    static class Handler implements Runnable, Comparable<Handler> {
+        private volatile static Set<Handler> allClients = new ConcurrentSkipListSet<Handler>();
+        private final Socket socket;
+        private final PrintStream out;
+        private final Scanner input;
+
+        Handler(Socket socket) throws IOException {
+            this.socket = socket;
+
+            InputStream is = socket.getInputStream();
+            input = new Scanner(is);
+
+            out = new PrintStream(socket.getOutputStream());
+            out.flush();
+
+            allClients.add(this);
+        }
+
+        public void run() {
+
+            out.println("Welcome You are connected to the Server....!");
+
+            String lastMessage;
+
+            do {
+                lastMessage = input.nextLine();
+
+                for (Handler h : Handler.allClients) {
+                    if (!h.equals(this)) {
+                        h.sendMessage();
+                    }
+                }
+            } while (lastMessage.indexOf("quit") != 0);
+
+            out.println("You are Disconnected");
+
+            out.close();
+
+            allClients.remove(this);
+        }
+
+        public synchronized void sendMessage() {
+            out.println("If You want to dicsconnect please type as quit");
+        }
+
+        public int compareTo(Handler other) {
+            return 1;
+        }
+    }
+}
